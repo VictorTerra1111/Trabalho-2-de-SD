5aa8915dad02fcf96ade345e44c5be2cc36ac576
João Victor Terra Pereira <123105471+VictorTerra1111@users.noreply.github.com>
2025-05-19 18:24:28 +0000

    fixed pontuacao.sv logic

--- HDL/pontuacao.sv
+++ HDL/pontuacao.sv
@@ -21,20 +21,19 @@ module pontuacao(
             game_over <= 0;
             p1wins_r <= 0;
             p2wins_r <= 0;
-            
             LED <= 16'b0;
         end else begin
             p1wins_r <= p1vic;
             p2wins_r <= p2vic;
 
-            if (!game_over) begin // se nao eh acabou
+            if (!game_over) begin // se nao acabou
                 if (p1vic_pulse && p1pontos < 7) begin
-                    LED[15 - p1pontos] <= 1'b1;
+                    LED <= LED | (16'b1 << (15 - p1pontos));
                     p1pontos <= p1pontos + 1;
                     if (p1pontos == 6) game_over <= 1;  // 7º ponto
                 end
                 if (p2vic_pulse && p2pontos < 7) begin
-                    LED[p2pontos] <= 1'b1;
+                    LED <= LED | (16'b1 << p2pontos);
                     p2pontos <= p2pontos + 1;
                     if (p2pontos == 6) game_over <= 1;  // 7º ponto
                 end
